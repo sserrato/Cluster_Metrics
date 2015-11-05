2783d4bab9862cf17a665d857c7ec190683f3500
Sergio Serrato <serrato@cal.berkeley.edu>
2015-11-04 17:20:31 -0800

    functional charting package install on heroku and local. able to classify via csv upload and manually route users to the rest.

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -67,7 +67,7 @@ class EmailsController < ApplicationController
   # GET /emails
   # GET /emails.json
   def index
-    @emails = Email.all
+    @emails = Email.all.order("id DESC")
   end
 
   # GET /emails/1
